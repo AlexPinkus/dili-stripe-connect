c991e0dc19cdc26f8be90a116c3b90998fb8d92a
alexpinkus <alexpinkus@hotmail.com>
2021-02-26 20:27:09 -0600

    Fixed redirection on active account

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,22 +1,45 @@
-import { Component } from '@angular/core';
+import { take } from 'rxjs/operators';
+
+import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
 import { AngularFireFunctions } from '@angular/fire/functions';
 import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {
-  errorMessage = '';
-  loading = false;
+export class LoginComponent implements OnInit {
+  loadingLink = false;
+  loadingUser = true;
+
   loginForm: FormGroup;
 
-  constructor(public auth: AngularFireAuth, private fns: AngularFireFunctions, private fb: FormBuilder) {
+  constructor(
+    public auth: AngularFireAuth,
+    private fns: AngularFireFunctions,
+    private fb: FormBuilder,
+    private afs: AngularFirestore,
+    private router: Router
+  ) {
     this.loginForm = this.fb.group({
       email: ['', Validators.compose([Validators.required, Validators.email])],
       password: ['', Validators.compose([Validators.required, Validators.minLength(6)])],
+    });
+  }
+
+  ngOnInit(): void {
+    this.auth.user.pipe(take(1)).subscribe(async (user) => {
+      if (user) {
+        await this.auth.signOut();
+        if (await this.isAccountActive(user.uid)) {
+          this.router.navigate(['success']);
+        }
+      }
+      this.loadingUser = false;
     });
   }
 
@@ -26,21 +49,31 @@ export class LoginComponent {
     }
     const email = this.loginForm.value.email;
     const password = this.loginForm.value.password;
+
     try {
-      await this.auth.signInWithEmailAndPassword(email, password);
+      const credentials = await this.auth.signInWithEmailAndPassword(email, password);
+      if (await this.isAccountActive(credentials.user.uid)) {
+        this.auth.signOut();
+        this.router.navigate(['success']);
+        return;
+      }
     } catch (error) {
       alert('Error iniciando sesión, verifique sus credenciales');
       return;
     }
 
-    this.loading = true;
+    this.loadingLink = true;
     const link = await this.fns.httpsCallable('stripeOnboardAccount')(null).toPromise();
-    this.loading = false;
     window.location.href = link;
   }
 
   logout(): void {
     this.auth.signOut();
+  }
+
+  async isAccountActive(uid: string): Promise<boolean> {
+    const owner = (await this.afs.collection('Owners').doc(uid).ref.get()).data() as any;
+    return !!owner && !!owner.accountId && owner.isAccountActive;
   }
 
   get email(): AbstractControl {
